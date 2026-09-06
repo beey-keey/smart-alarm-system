458386bc22d7b46d85513c033038fa5480df3cdd
Kircheva Boryana (LDC) <boryana.kircheva@liebherr.com>
2026-09-06 15:49:23 +0300

    Allow GitHub Pages frontend origin

--- smart_alarm_server.py
+++ smart_alarm_server.py
@@ -20,7 +20,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",
+                   "https://beey-keey.github.io",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
