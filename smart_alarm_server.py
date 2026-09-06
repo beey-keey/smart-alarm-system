568fe27d9851f623daafd0e2b9a42ab3695dfc36
Kircheva Boryana (LDC) <boryana.kircheva@liebherr.com>
2026-09-06 14:27:11 +0300

    Use cloud database connection

--- smart_alarm_server.py
+++ smart_alarm_server.py
@@ -43,10 +43,7 @@ JWT_ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
 SERVER_IP = os.getenv("SERVER_IP")
 PORT = os.getenv("PORT")
 
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 alarm_state = "ON"
 
@@ -97,12 +94,7 @@ def verify_token(
 # === DATABASE ===
 
 def save_alarm_to_db(alarm_type: str):
-    with psycopg.connect(
-        dbname=DB_NAME,
-        user=DB_USER,
-        host=DB_HOST,
-        port=DB_PORT,
-    ) as conn:
+    with psycopg.connect(DATABASE_URL) as conn:
         with conn.cursor() as cur:
             cur.execute(
                 "INSERT INTO alarm_events (type) VALUES (%s)",
@@ -111,12 +103,7 @@ def save_alarm_to_db(alarm_type: str):
 
 
 def get_alarms_from_db():
-    with psycopg.connect(
-        dbname=DB_NAME,
-        user=DB_USER,
-        host=DB_HOST,
-        port=DB_PORT,
-    ) as conn:
+    with psycopg.connect(DATABASE_URL) as conn:
         with conn.cursor() as cur:
             cur.execute(
                 """
